72a667753d7a9b11c3cc97105528d5475a6fe329
Selezneva Marina <petrukhina.marina2011@yandex.ru>
2022-04-28 12:18:11 +0300

    add files and workflow

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+from brain_games import cli
+
+
 def main():
-	print('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
 
 
+if __name__ == '__main__':
+    main()
 
-if __name__== '__main__':
-	main()
+cli.welcome_user()
