6b6fd55f7ab1ff0df03bfcb45f1440730c55b4b6
Selezneva Marina <petrukhina.marina2011@yandex.ru>
2022-06-03 10:07:11 +0300

    add edits review 6

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -17,8 +17,8 @@ def is_prime(number):
 
     if number <= 1:
         return False
-    else:
-        for d in range(2, int(math.sqrt(number) + 1)):
-            if number % d == 0:
-                return False
-        return True
+
+    for d in range(2, int(math.sqrt(number) + 1)):
+        if number % d == 0:
+            return False
+    return True
--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,13 +4,13 @@ RULE = 'What number is missing in the progression?'
 
 def get_task_answer():
     progression_length = randint(5, 9)
-    progression_range = randint(2, 10)
+    elements_difference = randint(2, 10)
     first_element = randint(1, 10)
     progress = []
     random_index = randint(0, progression_length - 1)
 
     for j in range(progression_length + 1):
-        progress.append(str(first_element + j * progression_range))
+        progress.append(str(first_element + j * elements_difference))
 
     correct_answer = progress[random_index]
     progress[random_index] = '..'
